8349b66d2387db08e4e669e33da6ab660ca6f3e8
akmal nawabi <akmalnawabi007@gmail.com>
2023-05-22 13:07:04 +0430

    first commit

---
+++ crud/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../crud/style/style.css">
+</head>
+<body>
+    <div class="userForm">
+        <form action="" method="post">
+            <label for="">First Name</label><input type="text" name="firstname" placeholder="Enter Your Name"><br>
+            <label for="">Username</label> <input type="text" name="username" placeholder="Enter Your username"><br>
+            <label for="">Email</label> <input type="email" name="email" placeholder="Enter Your email"><br>
+            <label for="">Password</label> <input type="text" name="password" placeholder="Enter Your password"><br>
+            <label for="">Confirm Password</label> <input type="text" name="confirm" placeholder="Confirm your password"><br>
+            <input type="submit" name="submit" value="Save">
+        </form>
+    </div>
+</body>
+</html>
